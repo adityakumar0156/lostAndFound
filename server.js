59eb3590176b9617050b0e30536cc3607353a793
Aditya Kumar <73734281+adityakumar0156@users.noreply.github.com>
2022-06-26 17:20:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const PORT=process.env.PORT;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
     res.sendFile('./index.html');
 });
 
-app.listen(9000, () => {
+app.listen(PORT || 9000, () => {
     console.log("app is listening");
 });
